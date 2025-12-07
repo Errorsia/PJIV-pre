2eb13a5052656fc59207926ce4d2b1d09a3ab993
Errorsia <Errorsia@outlook.com>
2025-12-07 22:05:33 +0800

    rework version; add update url

--- Jiv_build_config.py
+++ Jiv_build_config.py
@@ -1,6 +1,8 @@
 PROJECT_NAME = 'Jiyu Immunodeficiency Virus'
 MAJOR_VER, MINOR_VER = 0, 1
-VERSION = f'{MAJOR_VER}.{MINOR_VER}'
+RELEASE_STAGE = 'a1'
+VERSION = f'{MAJOR_VER}.{MINOR_VER}{RELEASE_STAGE}'
 FULL_VERSION = f'{PROJECT_NAME} v{VERSION}'
+UPDATE_URL = "https://api.github.com/repos/Errorsia/PJIV-pre/releases/latest"
 # CODE_NAME = ''
 # NICKNAME = ''
