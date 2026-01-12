4f3bc960710a2e23c1bda7ee52d97001d1ddb55c
Errorsia <Errorsia@outlook.com>
2026-01-12 19:54:53 +0800

    feat: RequireNameMixin

--- jiv/gui/gui.py
+++ jiv/gui/gui.py
@@ -208,6 +208,15 @@ class MainWidget(QWidget):
                 }
             """)
 
+class RequireNameMixin:
+    required_methods = ["set_name"]
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        for method in cls.required_methods:
+            if not hasattr(cls, method):
+                raise TypeError(f"{cls.__name__} must implement {method}()")
+
 
 class ToolPage(QWidget):
     ui_change = Signal(str, object)
